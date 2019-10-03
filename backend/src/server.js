2391cf20230a144c5f363911e7888e2431a107c8
sadkevin <kevinshibuya@hotmail.com>
2019-10-03 14:14:20 -0300

    backend done, frontend almost done

---
+++ backend/src/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const routes = require('./routes');
+
+const app = express();
+
+mongoose.connect('mongodb+srv://omnistack:omnistack@omnistack9-0g2sc.mongodb.net/test?retryWrites=true&w=majority', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+// GET, POST, PUT, DELETE
+// req.query = Acessar query params (para filtros)
+// req.params = Acessar route params (para edit, delete)
+// req.body = Acessar corpo da req (para create, edit)
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333);
